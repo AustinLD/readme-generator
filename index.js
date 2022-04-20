63464155c99503c563d13116bcb5f9840e52b6f5
Austin Donovan <AustinLDonovan@gmail.com>
2022-04-20 18:01:03 -0400

    adding questions

--- index.js
+++ index.js
@@ -5,7 +5,42 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your project title?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please write a short project description.',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What kind of license does your project have?',
+      choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ])
+  
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
